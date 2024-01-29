3cc0f4cc67f21adbed8fccb690e23b60e8565585
Ralph Uy <ralph_malvin.uy@lethbridgecollege.ca>
2024-01-29 11:47:29 -0700

    add files

---
+++ week04-Numbers/src/main/java/com/mycompany/week04/numbers/Week04Numbers.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.week04.numbers;
+
+/**
+ *
+ * @author s0542191
+ */
+public class Week04Numbers {
+
+    public static void main(String[] args) {
+        Byte one = 0;
+        byte primeOne = 0;
+        
+        String temp = one.toString();
+        String temp2 = Byte.toString(one);
+        
+        Byte temp3 = Byte.parseByte("FF", 16);
+        
+        System.out.println();
+    }
+}
