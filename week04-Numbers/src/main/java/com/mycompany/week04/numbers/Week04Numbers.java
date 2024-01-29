57a7d52c23a48d3a8db3222fb5488e5a60852f26
Ralph Uy <ralph_malvin.uy@lethbridgecollege.ca>
2024-01-29 12:28:55 -0700

    add class discussion files

--- week04-Numbers/src/main/java/com/mycompany/week04/numbers/Week04Numbers.java
+++ week04-Numbers/src/main/java/com/mycompany/week04/numbers/Week04Numbers.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.week04.numbers;
+
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,11 +15,30 @@ public class Week04Numbers {
     public static void main(String[] args) {
         Byte one = 0;
         byte primeOne = 0;
-        
+
         String temp = one.toString();
         String temp2 = Byte.toString(one);
+
+        Byte temp3 = Byte.parseByte("101", 2);
+
+        String s1 = "Hey";
+        String s2 = "Hi";
+
+        int a = s1.compareTo(s2);
+
+        // String;
+        String name = "Jhen Eric";
+
+        StringBuilder space = new StringBuilder(name);
+        space.append(" lives here.");
         
-        Byte temp3 = Byte.parseByte("FF", 16);
+        StringBuilder space2 = space.replace(10, 10, "don't ");
+
+        // Get input from a user
+        String inputName = JOptionPane.showInputDialog("Enter name:");
+        
+        String inputAge = JOptionPane.showInputDialog("Enter age:");
+        Integer age = Integer.parseInt(inputAge);
         
         System.out.println();
     }
