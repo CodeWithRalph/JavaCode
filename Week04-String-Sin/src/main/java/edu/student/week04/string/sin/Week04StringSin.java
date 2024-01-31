0273b43719be715bbce4df5f7ac5edea150f174d
Ralph Uy <ralph_malvin.uy@lethbridgecollege.ca>
2024-01-31 12:35:43 -0700

    add week 4 class files

---
+++ Week04-String-Sin/src/main/java/edu/student/week04/string/sin/Week04StringSin.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package edu.student.week04.string.sin;
+
+/**
+ *
+ * @author s0542191
+ */
+public class Week04StringSin {
+
+    public static void main(String[] args) {
+        
+        String word = "hilli lilly lilo";
+        String newWord = word.replace("i", "e");
+        System.out.println(newWord);
+
+    }
+}
